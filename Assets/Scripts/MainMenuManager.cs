19fc7720ec99d18d1f476295e40d6718a05f0c6a
Joan Sebastian Garcia <jsebastian.garcia1995@gmail.com>
2025-01-26 13:58:13 -0500

    update
    changes for level 3
    SFX, the hole level
    To Do
    tentacle animations

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -5,25 +5,8 @@ using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public Color colorA;
-    public Color colorB;
-    public float duration = 2f;
-    public RawImage backgroundImage;
-    
-    private float timer = 0f;
     public void StartGame()
     {
-        SceneManager.LoadScene("Level1");
-    }
-
-    private void FixedUpdate()
-    {
-        if (backgroundImage != null)
-        {
-            timer += Time.deltaTime / duration;
-            float t = Mathf.PingPong(timer, 1f);
-            
-            backgroundImage.material.color = Color.Lerp(colorA, colorB, t);
-        }
+        SceneManager.LoadScene("Level3");
     }
 }
